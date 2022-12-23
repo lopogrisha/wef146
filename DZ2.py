28ad9234c0ad857352cf294ca73b808198eb3278
lopogrisha <den159chik753@gmail.com>
2022-12-23 16:16:20 +0200

    Dz2

--- DZ2.py
+++ DZ2.py
@@ -22,9 +22,31 @@ class Cat:
         self.progress-0.1
     def is_alive(self):
         if self.progress<=0.5:
-            print("Відрахування...")
+            print("Викид на вулицю...")
             self.alive=False
         elif self.gladness<=0:
             print("Скучно....")
             self.alive=False
-        
+        elif self.progress>5:
+            print("Став дорослим")
+            self.alive=False
+
+
+    def end_of_day(self):
+        print(f"Радість = {self.gladness}")
+        print(f"Прогрес = {round(self.progress,2)}")
+    def live(self,day):
+        day ="Day" +str(day + "of" + self.name + "lafe")
+        print(f"{day:=^50}")
+        live_cube=random.randint(1,3)
+        if live_cube==1:
+            self.to_study()
+        elif live_cube==2:
+            self.to_sleep()
+        elif live_cube==3:
+            self.to_chill
+nick=Cat(name="Nick")
+for day in range(365):
+    if nick.alive==False:
+        break
+    nick.live(day)
